6c104cf9e81b9169b80da105d10257afb0f72192
Srinjoy100 <146570634+Srinjoy100@users.noreply.github.com>
2024-12-30 07:20:09 +0000

    Datatypes

---
+++ Basics/datatypes.js
@@ -0,0 +1,20 @@
+"use strict"//To use the new version of JS
+//alert("Hello");//We are using node not browser
+let number = 3;
+let name = "Raj"
+let isLogged=  true;
+let state = null;
+
+//Primitive Datatype
+//number => 2^53
+//bigInt
+// string => ""
+//boolen => true/false
+//null => standalone value type of its is object REMEMBER
+//undefined 
+// symbol =>unique 
+
+
+//object
+console.log(typeof(state));//object
+console.log(typeof(undefined));//undefined
