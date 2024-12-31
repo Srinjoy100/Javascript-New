be8d40f88c23d5ce5974b31409afa6fb7c4e834c
Srinjoy100 <146570634+Srinjoy100@users.noreply.github.com>
2024-12-31 14:58:01 +0000

    everything about datatypes

--- Basics/datatypes.js
+++ Basics/datatypes.js
@@ -2,7 +2,7 @@
 //alert("Hello");//We are using node not browser
 let number = 3;
 let name = "Raj"
-let isLogged=  true;
+let isLogged = true;
 let state = null;
 
 //Primitive Datatype
@@ -16,5 +16,44 @@ let state = null;
 
 
 //object
-console.log(typeof(state));//object
-console.log(typeof(undefined));//undefined
+console.log(typeof (state));//object
+console.log(typeof (undefined));//undefined
+
+
+/****************************************Interview *******************************************/
+/* Types:-
+    Primitive(Call By Value):-
+    1.String , 2.Number ,3.Boolean ,4.NULL ,5.Undefined 6.Symbol(Used to make an element unique),7.BigINT.
+    
+    Reference Type (Non - Primitive)(Call By Refenrence):-
+    1.Array,2.Objects ,3.Functions*/
+
+const score = 100 //Dynamically written Language ,doesnt need to define the data type
+
+const scoreVlaue = 100.33
+
+const isLoggedIn = false
+
+const temp = null
+
+let userEmail;//Garbage in C, Undefined in JS
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.table([id, anotherId, id === anotherId])
+
+const bigNum = 204256164330431461n
+
+
+const heroes = ["Iron-Man", "Hulk", "Thor"]
+
+let myObj = {
+    name: MYself,
+    age: "22"
+}
+
+const myFunction = function () {
+    console.log("Hello World")
+
+}
