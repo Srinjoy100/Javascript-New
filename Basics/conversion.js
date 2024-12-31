500555dc20ee72e0eab18d5134a9be1559daaa31
Srinjoy100 <146570634+Srinjoy100@users.noreply.github.com>
2024-12-31 05:08:50 +0000

    conversion

---
+++ Basics/conversion.js
@@ -0,0 +1,22 @@
+let score = undefined;
+//console.log(typeof score);
+
+let valuInNumber = Number(score)
+//console.log(typeof valuInNumber);
+//console.log(valuInNumber)
+
+/*NOTES:
+"33" => 33
+"33abc" => NaN
+true => 1 ,false =>0*/
+
+let isLoggedin = "Name";
+let booleanLogged = Boolean(isLoggedin);
+console.log(typeof (booleanLogged))
+console.log(booleanLogged)
+
+// " " =>false , "Name" => true
+let n = 45;
+let toString = String(n);
+console.log(toString);
+console.log(typeof(toString))
