c120945ee0b97596a5d879a0934094db35efbcec
Srinjoy100 <146570634+Srinjoy100@users.noreply.github.com>
2025-04-19 11:33:24 +0000

    Loop First

---
+++ Loops/first.js
@@ -0,0 +1,39 @@
+// for (let index = 0; index < 10; index++) {
+//     console.log(index);
+//     if(index==5)
+//     {
+//         console.log("It is 5")
+//     }
+// }
+
+// for (let index = 0; index < 10; index++) {
+//     for(let  j=0;j<=index;j++)
+//     {
+//         console.log(j);
+//     }
+//    // const element = array[index];
+//     console.log(index)
+// }
+
+// let myArray=["Flash","Batman","Superman"];
+// for (let index = 0; index < myArray.length; index++) {
+//     const element = myArray[index];
+//     console.log(element)
+    
+// }
+
+/***************Break & continue*/
+
+for(let i=0;i<10;i++)
+{
+    console.log(i);
+    if(i==3)
+        break;
+}
+
+for(let i=0;i<10;i++)
+    {
+        console.log(i);
+        if(i==3)
+            continue;
+    }
