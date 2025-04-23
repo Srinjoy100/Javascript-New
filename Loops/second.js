dc61cc9fda05c9f6e6580504270c7896302b9f96
Srinjoy100 <146570634+Srinjoy100@users.noreply.github.com>
2025-04-23 15:16:42 +0000

    LOOP part 2

---
+++ Loops/second.js
@@ -0,0 +1,21 @@
+
+// let i=0
+
+// while (i<11) {
+//     console.log(i);
+//     i++;
+// }
+// let a=0;
+// const myarr=["Spider","Ant","Wasp"];
+// while(a<myarr.length)
+// {
+//     console.log(`Value of Element is ${myarr[a]}`);
+//     a++;
+// }
+
+/*******************************Do While Loop******************************************* */
+let score=0;
+do{
+    console.log(score);
+    score++;
+}while(score<6);
